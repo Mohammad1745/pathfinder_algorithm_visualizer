42d72a303275ceebb41df0130d53eb3d57e2440b
Mohammad Ali <mdali2016.227@gmail.com>
2021-06-24 00:12:13 +0600

    [Add] reaching end node with dijkstra algorithm

---
+++ dijkstra.js
@@ -0,0 +1,60 @@
+let solvedNodes = []
+let unsolvedNodes = []
+
+function dijkstraSearch({row, column, startingPoint, endPoint}) {
+    solvedNodes.push({
+        position: startingPoint,
+        distance: 0,
+        prev: null,
+        solved: true,
+        weight: 1
+    })
+    activatePoint(startingPoint)
+
+    while (true) {
+    // for (let i=0; i<665; i++) {
+        let lastNode = solvedNodes[solvedNodes.length-1]
+        let nextNodePositions = [
+            [lastNode.position[0], lastNode.position[1]+1],
+            [lastNode.position[0], lastNode.position[1]-1],
+            [lastNode.position[0]+1, lastNode.position[1]],
+            [lastNode.position[0]-1, lastNode.position[1]],
+        ]
+        nextNodePositions.map(nextNodePosition => {
+            let isNodeValid = nextNodePosition[0]>=0 && nextNodePosition[0]<row && nextNodePosition[1]>=0 && nextNodePosition[1]<column
+            let isSolvedNode = solvedNodes.filter(node => node.position[0] === nextNodePosition[0] && node.position[1] === nextNodePosition[1]).length>0
+            if (!isSolvedNode && isNodeValid) {
+                let matchedUnsolvedNode = unsolvedNodes.filter(node => node.position[0] === nextNodePosition[0] && node.position[1] === nextNodePosition[1])
+                if (matchedUnsolvedNode[0]) {
+                    if (lastNode.distance+1 < matchedUnsolvedNode.distance) {
+                        matchedUnsolvedNode.distance = lastNode.distance+1
+                        matchedUnsolvedNode.prev = lastNode.position
+                    }
+                } else {
+                    unsolvedNodes.push({
+                        position: nextNodePosition,
+                        distance: lastNode.distance+1,
+                        prev: lastNode.position,
+                        solved: false,
+                        weight: 1
+                    })
+                }
+            }
+        })
+        unsolvedNodes.sort((a, b) => a.distance-b.distance)
+        let targetNode = unsolvedNodes.shift()
+        let matchedNode = solvedNodes.filter(node => node.position[0] === targetNode.position[0] && node.position[1] === targetNode.position[1]).length>0
+        if (!matchedNode) {
+            solvedNodes.push(targetNode)
+            activatePoint(targetNode.position, targetNode.distance)
+        }
+        if (targetNode.position[0]===endPoint[0]&&targetNode.position[1]===endPoint[1]) {
+            console.log('target reached: '+endPoint)
+            break
+        }
+    }
+    console.log('solved: '+solvedNodes.length)
+    solvedNodes.map(node => console.log(node.position))
+    console.log('unsolved: '+unsolvedNodes.length)
+    unsolvedNodes.map(node => console.log(node.position))
+}
--- script.js
+++ script.js
@@ -1,23 +1,53 @@
-let row = 30
-let column = 70
-
+let row = 20
+let column = 50
+let startingPoint = [Math.round(row/2), Math.round(row/2)]
+let endPoint = [Math.round(row/2), column-Math.round(row/2)]
 
 document.addEventListener('DOMContentLoaded', () => {
     plotGraph()
+    indicateStartingPoint()
+    indicateEndPoint()
+
+    document.querySelector('#visualize_btn').addEventListener('click', event => {
+        dijkstraSearch({row, column, startingPoint, endPoint})
+    })
 })
 
 function plotGraph() {
-    let nodeSize = 20;
     let graphBody = document.querySelector('#graph_body')
+    let nodeSize = Math.floor(graphBody.offsetWidth/column-1)
     for (let i=0; i<row; i++) {
         graphBody.insertAdjacentHTML('beforeend', `<div class="node-row row m-0" id="node_row_${i}" data-row="${i}"></div>`)
         let nodeRow = graphBody.querySelector(`#node_row_${i}`)
-        nodeSize = Math.floor((nodeRow.clientWidth-1)/column)
-        for (let j=0; j<column; j++){
+        for (let j=0; j<column; j++) {
             nodeRow.insertAdjacentHTML('beforeend', `<div class="node" id="node_${i}_${j}" data-row="${i}" data-column="${j}"></div>`)
             let node = nodeRow.querySelector(`#node_${i}_${j}`)
             node.style.width = nodeSize+"px"
             node.style.height = nodeSize+"px"
         }
     }
+}
+
+function activatePoint(point, distance=0) {
+    let node = document
+        .querySelector('#graph_body')
+        .querySelector(`#node_row_${point[0]}`)
+        .querySelector(`#node_${point[0]}_${point[1]}`)
+    node.classList.add('node-active')
+    node.innerHTML = distance+''
+}
+
+function indicateStartingPoint() {
+    let node = document
+        .querySelector('#graph_body')
+        .querySelector(`#node_row_${startingPoint[0]}`)
+        .querySelector(`#node_${startingPoint[0]}_${startingPoint[1]}`)
+    node.innerHTML = '>'
+}
+function indicateEndPoint() {
+    let node = document
+        .querySelector('#graph_body')
+        .querySelector(`#node_row_${endPoint[0]}`)
+        .querySelector(`#node_${endPoint[0]}_${endPoint[1]}`)
+    node.innerHTML = 'X'
 }
