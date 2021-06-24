cf604a44d6f50280759a3fa4e1cf1ddadf73c88d
Mohammad Ali <mdali2016.227@gmail.com>
2021-06-24 09:21:48 +0600

    [Add] equals method to check arrays

--- dijkstra.js
+++ dijkstra.js
@@ -2,8 +2,6 @@ let solvedNodes = []
 let unsolvedNodes = []
 
 function dijkstraSearch({row, column, startingPoint, endPoint}) {
-    solvedNodes = []
-    unsolvedNodes = []
     solvedNodes.push({
         position: startingPoint,
         distance: 0,
@@ -14,7 +12,6 @@ function dijkstraSearch({row, column, startingPoint, endPoint}) {
     activatePoint(startingPoint)
 
     while (true) {
-    // for (let i=0; i<665; i++) {
         let lastNode = solvedNodes[solvedNodes.length-1]
         let nextNodePositions = [
             [lastNode.position[0], lastNode.position[1]+1],
@@ -24,9 +21,9 @@ function dijkstraSearch({row, column, startingPoint, endPoint}) {
         ]
         nextNodePositions.map(nextNodePosition => {
             let isNodeValid = nextNodePosition[0]>=0 && nextNodePosition[0]<row && nextNodePosition[1]>=0 && nextNodePosition[1]<column
-            let isSolvedNode = solvedNodes.filter(node => node.position[0] === nextNodePosition[0] && node.position[1] === nextNodePosition[1]).length>0
+            let isSolvedNode = solvedNodes.filter(node => node.position.equals(nextNodePosition)).length>0
             if (!isSolvedNode && isNodeValid) {
-                let matchedUnsolvedNode = unsolvedNodes.filter(node => node.position[0] === nextNodePosition[0] && node.position[1] === nextNodePosition[1])
+                let matchedUnsolvedNode = unsolvedNodes.filter(node => node.position.equals(nextNodePosition))
                 if (matchedUnsolvedNode[0]) {
                     if (lastNode.distance+1 < matchedUnsolvedNode.distance) {
                         matchedUnsolvedNode.distance = lastNode.distance+1
@@ -45,23 +42,30 @@ function dijkstraSearch({row, column, startingPoint, endPoint}) {
         })
         unsolvedNodes.sort((a, b) => a.distance-b.distance)
         let targetNode = unsolvedNodes.shift()
-        let matchedNode = solvedNodes.filter(node => node.position[0] === targetNode.position[0] && node.position[1] === targetNode.position[1]).length>0
+        let matchedNode = solvedNodes.filter(node => node.position.equals(targetNode.position)).length>0
         if (!matchedNode) {
             solvedNodes.push(targetNode)
             activatePoint(targetNode.position, targetNode.distance)
         }
-        if (targetNode.position[0]===endPoint[0]&&targetNode.position[1]===endPoint[1]) {
+        if (targetNode.position.equals(endPoint)) {
             return extractShortestPath(solvedNodes, targetNode)
         }
     }
 }
 
-function extractShortestPath (solvedNodes, lastNode) {
+function extractShortestPath (lastNode) {
     let shortestPath = []
     while (lastNode) {
         shortestPath.unshift(lastNode.position)
         if (!lastNode.prev) break
-        lastNode = solvedNodes.filter(node => node.position[0]===lastNode.prev[0] && node.position[1]===lastNode.prev[1])[0]
+        lastNode = solvedNodes.filter(node => node.position.equals(lastNode.prev))[0]
     }
     return shortestPath
 }
+
+Array.prototype.equals = function(arr2) {
+    return (
+        this.length === arr2.length &&
+        this.every((value, index) => value === arr2[index])
+    );
+};
